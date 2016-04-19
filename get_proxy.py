940b9875bc61f951ca44053e2a06aa8b94ad3978
kirimaks <kirimaks@yahoo.com>
2016-04-19 20:02:06 +0300

    GetProxy class

--- get_proxy.py
+++ get_proxy.py
@@ -1,40 +1,37 @@
 import ConfigParser
 import psycopg2
-import logging
 
-class GetProxyTools(object):
-    def __init__(self, cursor):
-        self.cursor = cursor
+import os
 
-    def get_proxy(self): 
-        self.cursor.execute("SELECT * FROM proxy_list ORDER BY random() LIMIT 1")
-        out = self.cursor.fetchone()
-        return "{protocol}://{addr}:{port}".format(protocol = out[4].strip(), addr=out[1].strip(), port=out[2].strip())
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class GetProxy(object):
     def __init__(self, loglevel=30, *pargs, **kargs):
-        logging.basicConfig(level=loglevel)
-        logging.debug("Parse config")
-        
         iniparser = ConfigParser.ConfigParser()
-        iniparser.read("proxy_list.ini")
+        iniparser.read(os.path.join(BASE_DIR, "proxy_list.ini"))
 
         self.db_name = iniparser.get("Database", "database")
         self.db_user = iniparser.get("Database", "user")
         self.db_pass = iniparser.get("Database", "pass")
 
-    def __enter__(self):
-        logging.debug("Connect to database")
+        if __debug__: print("*** Connect to database ***")
         self.connect = psycopg2.connect("dbname={0} user={1} password={2}".format(self.db_name, self.db_user, self.db_pass))
         self.cursor = self.connect.cursor()
-        return GetProxyTools(self.cursor)
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        logging.debug("Close connection")
+    def __del__(self):
+        if __debug__: print("*** Close connection ***")
         self.connect.commit()
         self.cursor.close()
         self.connect.close()
 
-with GetProxy(loglevel=0) as gp:
-    for i in range(15):
-        print(gp.get_proxy())
+    def get_proxy(self): 
+        if __debug__: print("*** Generate new proxy ***")
+        self.cursor.execute("SELECT * FROM proxy_list ORDER BY random() LIMIT 1")
+        out = self.cursor.fetchone()
+        return "{protocol}://{addr}:{port}".format(protocol = out[4].strip(), addr=out[1].strip(), port=out[2].strip())
+
+
+if __name__ == "__main__":
+    px = GetProxy(0)
+    for i in range(5):
+        print(px.get_proxy())
